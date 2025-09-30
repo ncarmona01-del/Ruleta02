26c3a2cce0e66382ae7713082a1924c51a90f480
Taller <n.carmona01@ufromail.cl>
2025-09-30 15:59:46 -0300

    Ventanas

---
+++ src/main/java/Modelo/Usuario.java
@@ -0,0 +1,28 @@
+package Modelo;
+
+public class Usuario {
+    private String nombreUsuario;
+    private String contraseña;
+
+    public Usuario(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+}
+
+
+
