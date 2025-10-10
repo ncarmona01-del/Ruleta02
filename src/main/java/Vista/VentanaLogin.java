aa8301c565962a61add11eb8b025c3f62ada75a3
Taller <n.carmona01@ufromail.cl>
2025-10-10 09:30:29 -0300

    ventana login casi lista falta validacion de credenciales

--- src/main/java/Vista/VentanaLogin.java
+++ src/main/java/Vista/VentanaLogin.java
@@ -1,8 +1,11 @@
 package Vista;
+import Controlador;
 
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -29,25 +32,26 @@ public class VentanaLogin {
         campoUsuario.setBounds(150, 40, 200, 25);
         ventana.add(campoUsuario);
 
-        JPasswordField campoContrasena = new JPasswordField(); // Use JPasswordField for security
-        campoContrasena.setBounds(150, 80, 200, 25);
-        ventana.add(campoContrasena);
+        JPasswordField campoContraseña = new JPasswordField(); // Use JPasswordField for security
+        campoContraseña.setBounds(150, 80, 200, 25);
+        ventana.add(campoContraseña);
 
         JButton botonIniciarSesion = new JButton("Iniciar Sesión");
         botonIniciarSesion.setBounds(150, 130, 150, 30);
         ventana.add(botonIniciarSesion);
 
-        JLabel inicioLogrado = new JLabel("");
+        JLabel inicioLogrado = new JLabel("Inicio de de Sesion Exitoso");
         inicioLogrado.setBounds(50, 180, 300, 25);
         inicioLogrado.setForeground(Color.GREEN);
-        ventana.add(inicioLogrado);
+
+        JLabel inicioFallido = new JLabel("Usuario o Contraseña incorrectos");
+        inicioFallido.setBounds(50, 180, 300, 25);
+        inicioFallido.setForeground(Color.RED);
 
         JButton botonRegistrar = new JButton("Registrar Nueva Cuenta");
         botonRegistrar.setBounds(50, 200, 150, 30);
         botonRegistrar.setForeground(Color.WHITE);
         ventana.add(botonRegistrar);
-
-
         KeyAdapter enterKeyAdapter = new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
